c2de59ada9d92634b734d3cce0a4fcfcfa2606e3
Laura Illouz <laura.hortense@gmail.com>
2021-07-05 16:24:52 +0200

    Exercise34

---
+++ exercice34/js/main.js
@@ -0,0 +1,10 @@
+$(".btn").click(() => {
+    let urlCountry = $("input").val();
+    $.ajax({
+        url: `https://restcountries.eu/rest/v2/name/${urlCountry}`,
+        success: function (data, status, response) {
+            $("#country").html(data[0].name);
+            $("#capital").html(data[0].capital);
+        }
+    })
+})
